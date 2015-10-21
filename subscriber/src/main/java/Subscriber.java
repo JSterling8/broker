ac3baa56b1b7265bc0c2dbc02555ad8e72bd7e1a
JSterling8 <JSterling8@gmail.com>
2015-10-21 15:55:30 +0100

    Moved some values into constants

--- subscriber/src/main/java/Subscriber.java
+++ subscriber/src/main/java/Subscriber.java
@@ -12,11 +12,16 @@ import java.nio.charset.CharsetDecoder;
  * Created by anon on 21/10/2015.
  */
 public class Subscriber {
+
+    public static final String BROKER_HOST = "127.0.0.1";
+    public static final int BROKER_SUBSCRIBER_PORT = 8078;
+    public static final String DEFAULT_CHARSET = "ISO-8859-1";
+
     public static void main(String[] args) throws IOException, ClassNotFoundException, InterruptedException {           //TODO Try/catch properly
         while (true) {
             SocketChannel socketChannel = SocketChannel.open();
             socketChannel.configureBlocking(true);
-            socketChannel.connect(new InetSocketAddress("127.0.0.1", 8078));
+            socketChannel.connect(new InetSocketAddress(BROKER_HOST, BROKER_SUBSCRIBER_PORT));
 
             String message = "Unmodified";
             ByteBuffer byteBuffer = ByteBuffer.allocate(512);
@@ -33,7 +38,7 @@ public class Subscriber {
                 }
 
                 byteBuffer.flip();
-                Charset charset = Charset.forName("ISO-8859-1");
+                Charset charset = Charset.forName(DEFAULT_CHARSET);
                 CharsetDecoder decoder = charset.newDecoder();
 
                 message = decoder.decode(byteBuffer).toString();

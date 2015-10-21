b8378baffc882aa31d65a1766c4dbd9e5442d756
JSterling8 <JSterling8@gmail.com>
2015-10-21 15:46:19 +0100

    Cleaned up broker code by splitting out into separate methods

--- broker/main/java/MessageBroker.java
+++ broker/main/java/MessageBroker.java
@@ -32,122 +32,134 @@ public class MessageBroker {
     private ConcurrentLinkedQueue<String> pendingMessages = new ConcurrentLinkedQueue<String>();
 
     public MessageBroker(){
-            Selector selector = null;
+        Selector selector = null;
+        try {
+            selector = Selector.open();
+        } catch (IOException e) {
+            LOGGER.error("Failed to open Selector", e);
+        }
+
+        int[] ports = {PUBLISHER_PORT, SUBCRIBER_PORT};
+
+        configureServerSocketChannels(selector);
+
+        while(true) {
             try {
-                selector = Selector.open();
+                selector.select();
             } catch (IOException e) {
-                LOGGER.error("Failed to open Selector", e);
+                LOGGER.error("Failed to get set of keys from Selector", e);
+                System.exit(-1);                                                // TODO Maybe let it fail a set amount of times before exiting?
             }
 
-            int[] ports = {PUBLISHER_PORT, SUBCRIBER_PORT};
+            Iterator<SelectionKey> selectedKeys = selector.selectedKeys().iterator();
+            while (selectedKeys.hasNext()) {
+                SelectionKey selectedKey = selectedKeys.next();
 
-            ServerSocketChannel publisherServerSocketChannel = null;
-            try {
-                publisherServerSocketChannel = ServerSocketChannel.open();
-                publisherServerSocketChannel.configureBlocking(false);
-                publisherServerSocketChannel.socket().bind(new InetSocketAddress(HOSTNAME, PUBLISHER_PORT));
-                publisherServerSocketChannel.register(selector, SelectionKey.OP_ACCEPT);
-            } catch (IOException e) {
-                LOGGER.error("Failed to open publisher ServerSocketChannel", e);
-            }
-
-            try {
-                ServerSocketChannel subscriberServerSocketChannel = ServerSocketChannel.open();
-                subscriberServerSocketChannel.configureBlocking(false);
-                subscriberServerSocketChannel.socket().bind(new InetSocketAddress(HOSTNAME, SUBCRIBER_PORT));
-                subscriberServerSocketChannel.register(selector, SelectionKey.OP_ACCEPT);
-            } catch (IOException e) {
-                LOGGER.error("Failed to open subscriber ServerSocketChannel", e);
-            }
-
-            while(true) {
-                try {
-                    selector.select();
-                } catch (IOException e) {
-                    LOGGER.error("Failed to get set of keys from Selector", e);
-                    System.exit(-1);                                                // TODO Maybe let it fail a set amount of times before exiting?
-                }
-
-                Iterator<SelectionKey> selectedKeys = selector.selectedKeys().iterator();
-                while (selectedKeys.hasNext()) {
-                    SelectionKey selectedKey = selectedKeys.next();
-
-                    if (selectedKey.isAcceptable()) {
-                        SocketChannel socketChannel = null;
+                if (selectedKey.isAcceptable()) {
+                    SocketChannel socketChannel = null;
+                    try {
+                        socketChannel = ((ServerSocketChannel) selectedKey.channel()).accept();
+                    } catch (IOException e) {
+                        LOGGER.error("Failed to accept ServerSocketChannel connection.", e);
+                    }
+                    if(socketChannel != null) {
                         try {
-                            socketChannel = ((ServerSocketChannel) selectedKey.channel()).accept();
+                            socketChannel.configureBlocking(false);
                         } catch (IOException e) {
-                            LOGGER.error("Failed to accept ServerSocketChannel connection.", e);
+                            LOGGER.error("Failed to configure blocking on SocketChannel", e);
                         }
-                        if(socketChannel != null) {
-                            try {
-                                socketChannel.configureBlocking(false);
-                            } catch (IOException e) {
-                                LOGGER.error("Failed to configure blocking on SocketChannel", e);
+                        Socket socket = socketChannel.socket();
+
+                        if(socket.getLocalPort() == PUBLISHER_PORT){
+                            ByteBuffer byteBuffer = ByteBuffer.allocate(512);
+                            String message = null;
+
+                            message = getMessageFromPublisher(socketChannel, byteBuffer, message);
+
+                            if(StringUtils.isNotBlank(message)){
+                                sendMessageToSubscribers(message);
                             }
-                            Socket socket = socketChannel.socket();
 
-                            if(socket.getLocalPort() == PUBLISHER_PORT){
-                                ByteBuffer byteBuffer = ByteBuffer.allocate(512);
-                                String message = null;
-
-                                while(byteBuffer.hasRemaining()){
-                                    try {
-                                        Thread.sleep(500);                      // FIXME - Find way to get message without sleeping...
-                                    } catch (InterruptedException e) {
-                                        e.printStackTrace();
-                                    }
-                                    int bytesRead = 0;
-                                    try {
-                                        bytesRead = socketChannel.read(byteBuffer);
-                                    } catch (IOException e) {
-                                        LOGGER.error("Failed to read byteBuffer", e);
-                                    }
-                                    if(bytesRead == -1){
-                                        try {
-                                            socketChannel.close();
-                                        } catch (IOException e) {
-                                            LOGGER.error("Failed to close SocketChannel");
-                                        }
-                                        continue;
-                                    }
-
-                                    byteBuffer.flip();
-                                    Charset charset = Charset.forName("ISO-8859-1");
-                                    CharsetDecoder decoder = charset.newDecoder();
-
-                                    try {
-                                        message = decoder.decode(byteBuffer).toString();
-                                    } catch (CharacterCodingException e) {
-                                        LOGGER.error("Failed to decode message from producer", e);
-                                    }
-                                }
-
-                                if(StringUtils.isNotBlank(message)){
-                                    sendToSubscribers(message);
-                                }
-
-                                break;
-                            } else {
-                                socketChannels.add(socketChannel);
-                                if(pendingMessages.size() > 0){
-                                    Iterator iterator = pendingMessages.iterator();
-
-                                    while(iterator.hasNext()){
-                                        String message = (String) iterator.next();
-                                        sendToSubscribers(message);
-                                        iterator.remove();
-                                    }
+                            break;
+                        } else {
+                            socketChannels.add(socketChannel);
+                            if(pendingMessages.size() > 0){
+                                Iterator iterator = pendingMessages.iterator();
+                                while(iterator.hasNext()){
+                                    String message = (String) iterator.next();
+                                    sendMessageToSubscribers(message);
+                                    iterator.remove();
                                 }
                             }
                         }
                     }
                 }
             }
-
+        }
     }
 
-    public void sendToSubscribers(String message){
+    private String getMessageFromPublisher(SocketChannel socketChannel, ByteBuffer byteBuffer, String message) {
+        while(byteBuffer.hasRemaining()){
+            try {
+                Thread.sleep(500);                      // FIXME - Find way to get full message without sleeping...
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+            int bytesRead = 0;
+            try {
+                bytesRead = socketChannel.read(byteBuffer);
+            } catch (IOException e) {
+                LOGGER.error("Failed to read byteBuffer", e);
+            }
+            if(bytesRead == -1){
+                try {
+                    socketChannel.close();
+                } catch (IOException e) {
+                    LOGGER.error("Failed to close SocketChannel");
+                }
+                continue;
+            }
+
+            byteBuffer.flip();
+
+            message = decodeMessage(byteBuffer, message);
+        }
+        return message;
+    }
+
+    private void configureServerSocketChannels(Selector selector) {
+        ServerSocketChannel publisherServerSocketChannel = null;
+        try {
+            publisherServerSocketChannel = ServerSocketChannel.open();
+            publisherServerSocketChannel.configureBlocking(false);
+            publisherServerSocketChannel.socket().bind(new InetSocketAddress(HOSTNAME, PUBLISHER_PORT));
+            publisherServerSocketChannel.register(selector, SelectionKey.OP_ACCEPT);
+        } catch (IOException e) {
+            LOGGER.error("Failed to open publisher ServerSocketChannel", e);
+        }
+
+        try {
+            ServerSocketChannel subscriberServerSocketChannel = ServerSocketChannel.open();
+            subscriberServerSocketChannel.configureBlocking(false);
+            subscriberServerSocketChannel.socket().bind(new InetSocketAddress(HOSTNAME, SUBCRIBER_PORT));
+            subscriberServerSocketChannel.register(selector, SelectionKey.OP_ACCEPT);
+        } catch (IOException e) {
+            LOGGER.error("Failed to open subscriber ServerSocketChannel", e);
+        }
+    }
+
+    private String decodeMessage(ByteBuffer byteBuffer, String message) {
+        Charset charset = Charset.forName("ISO-8859-1");
+        CharsetDecoder decoder = charset.newDecoder();
+        try {
+            message = decoder.decode(byteBuffer).toString();
+        } catch (CharacterCodingException e) {
+            LOGGER.error("Failed to decode message from producer", e);
+        }
+        return message;
+    }
+
+    public void sendMessageToSubscribers(String message){
         CharsetEncoder encoder = Charset.forName("ISO-8859-1").newEncoder();
 
         if(socketChannels.size() > 0) {

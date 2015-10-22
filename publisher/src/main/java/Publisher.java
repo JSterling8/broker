0dfc64e3b5b033e0a64ea53bbd7cf5ae910086a7
JSterling8 <JSterling8@gmail.com>
2015-10-22 21:40:58 +0100

    Split up connecting and message sending in Publisher

--- publisher/src/main/java/Publisher.java
+++ publisher/src/main/java/Publisher.java
@@ -1,3 +1,4 @@
+import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -17,32 +18,21 @@ import java.util.UUID;
 public class Publisher {
     private static final Logger LOGGER = LoggerFactory.getLogger(Publisher.class);
 
+    private SocketChannel socketChannel;
+
     public void connect(){
         try {
-            SocketChannel socketChannel = SocketChannel.open();
+            socketChannel = SocketChannel.open();
             socketChannel.configureBlocking(true);
             socketChannel.connect(new InetSocketAddress(ServerSettings.BROKER_HOSTNAME, ServerSettings.PUBLISHER_PORT));
 
             while(socketChannel.isConnectionPending()){
                 try {
-                    Thread.sleep(100);
+                    Thread.sleep(50);
                 } catch (InterruptedException e) {
                     e.printStackTrace();
                 }
             }
-
-            CharsetEncoder encoder = Charset.forName("ISO-8859-1").newEncoder();
-
-            CustomObject customObject = new CustomObject("A test publisher message",
-                    new UUID(System.currentTimeMillis(), System.currentTimeMillis() - 41134234l));
-
-            ObjectMapper objectMapper = new ObjectMapper();
-            String jsonEncodedObject = objectMapper.writeValueAsString(customObject);
-
-            LOGGER.debug("Sending CustomObject with message: " + customObject.getMessage() +
-                            "\nAnd UUID: " + customObject.getId());
-            socketChannel.write(encoder.encode(CharBuffer.wrap(jsonEncodedObject)));
-
         } catch (CharacterCodingException e1) {
             e1.printStackTrace();
         } catch (IOException e1) {
@@ -50,9 +40,33 @@ public class Publisher {
         }
     }
 
+    public void sendCustomObject(){
+        CharsetEncoder encoder = Charset.forName("ISO-8859-1").newEncoder();
+
+        CustomObject customObject = new CustomObject("A test publisher message",
+                new UUID(System.currentTimeMillis(), System.currentTimeMillis() - 41134234l));
+
+        ObjectMapper objectMapper = new ObjectMapper();
+        String jsonEncodedObject = null;
+        try {
+            jsonEncodedObject = objectMapper.writeValueAsString(customObject);
+        } catch (JsonProcessingException e) {
+            e.printStackTrace();
+        }
+
+        LOGGER.debug("Sending CustomObject with message: " + customObject.getMessage() +
+                "\nAnd UUID: " + customObject.getId());
+        try {
+            socketChannel.write(encoder.encode(CharBuffer.wrap(jsonEncodedObject)));
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
     public static void main(String[] args){
-        Publisher main = new Publisher();
-        main.connect();
+        Publisher publisher = new Publisher();
+        publisher.connect();
+        publisher.sendCustomObject();
     }
 
 }
